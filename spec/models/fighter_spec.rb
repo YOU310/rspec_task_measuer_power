0abe6f0f3380e1535534ce155fd1f50e7b599c8a
YOU <youmaya310@gmail.com>
2021-05-11 22:24:15 +0900

    letを使ってコードを簡略化

--- spec/models/fighter_spec.rb
+++ spec/models/fighter_spec.rb
@@ -2,23 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Fighter, type: :model do
   describe "戦闘力診断" do
+    let(:params){{name: "satou"}}
     it "戦闘力5:戦闘力診断ができること" do
-      fighter = Fighter.new(name: "satou", combat_power: 5)
+      params.merge!(combat_power: 5)
+      fighter = Fighter.new(params)
       expect(fighter.measure_power).to eq "satouさんの戦闘力は一般人です"
     end
 
     it "戦闘力130:戦闘力診断ができること" do
-      fighter = Fighter.new(name: "satou", combat_power: 130)
+      params.merge!(combat_power: 130)
+      fighter = Fighter.new(params)
       expect(fighter.measure_power).to eq "satouさんの戦闘力は亀仙人クラスです"
     end
 
     it "戦闘力1000:戦闘力診断ができること" do
-      fighter = Fighter.new(name: "satou", combat_power: 1000)
+      params.merge!(combat_power: 1000)
+      fighter = Fighter.new(params)
       expect(fighter.measure_power).to eq "satouさんの戦闘力は下級サイヤ人クラスです"
     end 
 
     it "戦闘力5000:戦闘力診断ができること" do
-      fighter = Fighter.new(name: "satou", combat_power: 5000)
+      params.merge!(combat_power: 5000)
+      fighter = Fighter.new(params)
       expect(fighter.measure_power).to eq "satouさんの戦闘力はエリートサイヤ人クラスです"
     end
   end
